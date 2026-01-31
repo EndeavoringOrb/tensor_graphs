e4af12f9ef47128c39638fc1f47cf6f764959c28
EndeavoringOrb <azbelikoff@wpi.edu>
2026-01-31 16:02:41 -0500

    make concat test use attrs

--- tests/atomic_ops/test_concat.py
+++ tests/atomic_ops/test_concat.py
@@ -25,15 +25,13 @@ def test_concat_matrices_axis_0():
     # (2, 3) + (1, 3) -> (3, 3)
     a = TensorNode(OpType.INPUT, (2, 3), DType.FP32, [], "a")
     b = TensorNode(OpType.INPUT, (1, 3), DType.FP32, [], "b")
-    axis = TensorNode(OpType.INPUT, (1,), DType.INT32, [], "axis")
 
-    concat_node = TensorNode(OpType.CONCAT, (3, 3), DType.FP32, [a, b, axis], "concat")
+    concat_node = TensorNode(OpType.CONCAT, (3, 3), DType.FP32, [a, b], "concat", attrs={"axis": 0})
 
     val_a = np.zeros((2, 3), dtype=np.float32)
     val_b = np.ones((1, 3), dtype=np.float32)
-    val_axis = np.array([0], dtype=np.int32)
 
-    res = evaluate_graph(concat_node, {"a": val_a, "b": val_b, "axis": val_axis})
+    res = evaluate_graph(concat_node, {"a": val_a, "b": val_b})
 
     expected = np.array([[0, 0, 0], [0, 0, 0], [1, 1, 1]], dtype=np.float32)
 
@@ -46,15 +44,13 @@ def test_concat_matrices_axis_1():
     # (2, 2) + (2, 1) -> (2, 3)
     a = TensorNode(OpType.INPUT, (2, 2), DType.FP32, [], "a")
     b = TensorNode(OpType.INPUT, (2, 1), DType.FP32, [], "b")
-    axis = TensorNode(OpType.INPUT, (1,), DType.INT32, [], "axis")
 
-    concat_node = TensorNode(OpType.CONCAT, (2, 3), DType.FP32, [a, b, axis], "concat")
+    concat_node = TensorNode(OpType.CONCAT, (2, 3), DType.FP32, [a, b], "concat", attrs={"axis": 1})
 
     val_a = np.full((2, 2), 1.0, dtype=np.float32)
     val_b = np.full((2, 1), 2.0, dtype=np.float32)
-    val_axis = np.array([1], dtype=np.int32)
 
-    res = evaluate_graph(concat_node, {"a": val_a, "b": val_b, "axis": val_axis})
+    res = evaluate_graph(concat_node, {"a": val_a, "b": val_b})
 
     expected = np.array([[1, 1, 2], [1, 1, 2]], dtype=np.float32)
 
