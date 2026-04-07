a49bf13b484b3bf912a3cbdcbd862f0cdd8e1002
EndeavoringOrb <azbelikoff@wpi.edu>
2026-04-07 11:20:21 -0400

    remove remove contiguous rule

--- tensor_graphs_cpp/core/planner.hpp
+++ tensor_graphs_cpp/core/planner.hpp
@@ -1122,7 +1122,6 @@ private:
         Rewrite::DivAddRule dar;
         Rewrite::CopyToContiguousReorderRule ccr;
         Rewrite::CopyToScatterReorderRule csr;
-        Rewrite::RemoveContiguousRule rcr;
         RemoveIdentityRepeatRule rirr;
         AddIdentityRepeatRule airr;
 
@@ -1138,7 +1137,6 @@ private:
         rules.emplace_back(std::make_unique<GraphRewriteRuleAdapter>(&dar));
         rules.emplace_back(std::make_unique<GraphRewriteRuleAdapter>(&ccr));
         rules.emplace_back(std::make_unique<GraphRewriteRuleAdapter>(&csr));
-        rules.emplace_back(std::make_unique<GraphRewriteRuleAdapter>(&rcr));
         rules.emplace_back(std::make_unique<RemoveIdentityRepeatRule>());
         rules.emplace_back(std::make_unique<AddIdentityRepeatRule>());
         rules.emplace_back(std::make_unique<FusionRule>());
