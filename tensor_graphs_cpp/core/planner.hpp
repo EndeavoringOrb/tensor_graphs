41f5bb0e68da225f4024b8fe11c82837754c790c
EndeavoringOrb <azbelikoff@wpi.edu>
2026-04-24 15:24:55 -0400

    partial fix

--- tensor_graphs_cpp/core/planner.hpp
+++ tensor_graphs_cpp/core/planner.hpp
@@ -389,9 +389,9 @@ private:
         rules.emplace_back(std::make_unique<ContiguousOfCopyTo>());
         rules.emplace_back(std::make_unique<ContiguousElimination>());
         rules.emplace_back(std::make_unique<ConstantFolding>());
-        rules.emplace_back(std::make_unique<InfinityDomination>());
-        rules.emplace_back(std::make_unique<SlicePushBackward>());
-        rules.emplace_back(std::make_unique<ScatterPushForward>());
+        // rules.emplace_back(std::make_unique<InfinityDomination>());
+        // rules.emplace_back(std::make_unique<SlicePushBackward>());
+        // rules.emplace_back(std::make_unique<ScatterPushForward>());
         // rules.emplace_back(std::make_unique<DistributiveProperty>());
 
         std::vector<uint32_t> protectedVec(protectedEClasses.begin(), protectedEClasses.end());
